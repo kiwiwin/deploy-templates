03a4f7852a531b519c48ba498262ee79e3fe0729
kiwi <jcwen@thoughtworks.com>
2014-07-28 23:59:21 +0800

    add comment about puppet postgresql configuration file

--- puppet-templates/modules/postgresql/manifests/init.pp
+++ puppet-templates/modules/postgresql/manifests/init.pp
@@ -9,4 +9,25 @@ class postgresql(
 	package { 'postgresql-9.3':
 		require => Anchor['postgresql::repo']
 	}	
+
+	#notice restart server when configuration file changed
+	#see documentation of puppet cookbook: http://www.puppetcookbook.com/posts/restart-a-service-when-a-file-changes.html
+	service { 'postgresql':
+		ensure => 'running',
+		enable => 'true',
+		require => Package['postgresql-9.3']
+	}
+
+	file { '/etc/postgresql/9.3/main/pg_hba.conf':
+		notify => Service['postgresql'],
+		require => Package['postgresql-9.3'],
+		source => "/vagrant/modules/postgresql/files/pg_hba.conf"
+	}
+
+	#even we may change two files at same time, but we will just notify the service to restart once
+	file { '/etc/postgresql/9.3/main/postgresql.conf':
+		notify => Service['postgresql'],
+		require => Package['postgresql-9.3'],
+		source => "/vagrant/modules/postgresql/files/postgresql.conf"
+	}
 }
